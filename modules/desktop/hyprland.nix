2a8441ee51487699efb0047034be4a559353de98
Thierry Ghélew <tlux@ghelew.net>
2023-08-20 10:47:12 +0200

    fix garbage collection settings

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -82,11 +82,6 @@ in {
     AllowHybridSleep=yes
   '';
 
-    nix.settings = {
-      substituters = ["https://hyprland.cachix.org"];	# Install cached version so rebuild should not be required
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-    };
-
     systemd.user.services."dunst" = {
       enable = true;
       description = "";
