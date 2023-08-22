d52a59bbcf30c4d6656ff9433702f03846495dc3
Thierry Ghélew <tlux@ghelew.net>
2023-08-22 22:48:40 +0200

    Adapted scripts and rofi apps

--- modules/desktop/apps/rofi.nix
+++ modules/desktop/apps/rofi.nix
@@ -7,6 +7,7 @@ let
   pkgRofi = if config.services.xserver.enable
              then pgks.rofi
              else pkgs.rofi-wayland;
+  configDir = config.nixos-config.configDir;
 
 in {
   options.modules.desktop.apps.rofi = {
@@ -24,40 +25,12 @@ in {
       (writeScriptBin "rofi" ''
         #!${stdenv.shell}
         exec ${pkgRofi}/bin/rofi -terminal "${config.modules.desktop.term.default}" -m -1 "$@"
-      '')
+       '')
+      pwgen # for rofi-pass
 
-      # Fake rofi dmenu entries
-      (makeDesktopItem {
-        name = "rofi-browsermenu";
-        desktopName = "Open Bookmark in Browser";
-        icon = "bookmark-new-symbolic";
-        exec = "${config.nixos-config.binDir}/rofi/browsermenu";
-      })
-      (makeDesktopItem {
-        name = "rofi-browsermenu-history";
-        desktopName = "Open Browser History";
-        icon = "accessories-clock";
-        exec = "${config.nixos-config.binDir}/rofi/browsermenu history";
-      })
-      (makeDesktopItem {
-        name = "rofi-filemenu";
-        desktopName = "Open Directory in Terminal";
-        icon = "folder";
-        exec = "${config.nixos-config.binDir}/rofi/filemenu";
-      })
-      (makeDesktopItem {
-        name = "rofi-filemenu-scratch";
-        desktopName = "Open Directory in Scratch Terminal";
-        icon = "folder";
-        exec = "${config.nixos-config.binDir}/rofi/filemenu -x";
-      })
-
-      (makeDesktopItem {
-        name = "lock-display";
-        desktopName = "Lock screen";
-        icon = "system-lock-screen";
-        exec = "${config.nixos-config.binDir}/zzz";
-      })
     ];
+    home.configFile = {
+      "rofi/scripts" = {source ="${configDir}/rofi/scripts"; recursive = "true"; };
+    };
   };
 }
