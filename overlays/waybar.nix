30657eb5aca42a6a1e2b01416b6098cbd98c7013
Thierry Ghélew <tlux@ghelew.net>
2023-09-29 19:21:05 +0200

    Get latest version of waybar

---
+++ overlays/waybar.nix
@@ -0,0 +1,12 @@
+final: prev:
+{
+waybar = final.waybar.overrideAttrs (oldAttrs: {
+  src = final.fetchFromGithub {
+    owner = "Alexays";
+    repo = "waybar";
+    rev = "4c0347d9f243322352a18bdf2c4d4bbcde37163e";
+    sha256 = "sha256-Y4Gb1RH1ShDpH1w0xmhFM0UjdecP6JXbUo50mwu5kek=";
+  };
+  mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+});
+}
