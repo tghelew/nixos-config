4d202b9fcd3dd6f3b006bea58e051f47b0d481da
Thierry Ghélew <tlux@ghelew.net>
2023-11-20 21:36:47 +0100

    general: adapted module for darwin

--- modules/all/desktop/graphics.nix
+++ modules/all/desktop/graphics.nix
@@ -6,7 +6,10 @@ with lib;
 with lib.my;
 let cfg = config.modules.desktop.media.graphics;
     configDir = config.nixos-config.configDir;
-    withWayland = config.programs.hyprland.enable || config.programs.sway.enable;
+    withWayland = pkgs.stdenv.isLinux &&
+                  (config.programs.hyprland.enable || config.programs.sway.enable);
+    isDarwin = pkgs.stdenv.isDarwin;
+    isLinux = pkgs.stdenv.isLinux;
 in {
   options.modules.desktop.media.graphics = {
     enable         = mkBoolOpt false;
@@ -18,32 +21,24 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = with pkgs;
-      (if cfg.tools.enable then [
-        font-manager   # so many damned fonts...
-        imagemagick    # for image manipulation from the shell
-      ] ++ (if withWayland then [ swayimg ] else [])
-       else []) ++
+      (if cfg.tool.enable then [ imagemagick ] else []) ++
+
+      (if cfg.tools.enable  && isLinux then [font-manager] else []) ++
+
+      (if withWayland then [ swayimg ] else []) ++
 
       # replaces illustrator & indesign
-      (if cfg.illustrator.enable then [
-        unstable.inkscape
-      ] else []) ++
+      (if cfg.illustrator.enable then [unstable.inkscape] else []) ++
 
       # Replaces photoshop
-      (if cfg.photoshop.enable then [
-        gimp
-      ] else []) ++
+      (if cfg.photoshop.enable then [gimp] else []) ++
 
       # 3D modelling
-      (if cfg.threed.enable then [
-        blender
-      ] else []);
+      (if cfg.threed.enable then [blender] else []);
 
     home.configFile = {
       "GIMP/2.10" = mkIf cfg.photoshop.enable { source = "${configDir}/gimp"; recursive = true; };
       "swayimg"   = mkIf (cfg.tools.enable && withWayland) { source = "${configDir}/swayimg"; };
     };
-
-
   };
 }
