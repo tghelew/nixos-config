19e1182876f811cefb1175385e110fe5518b3130
Thierry Ghélew <tlux@ghelew.net>
2023-12-06 11:10:10 +0100

    host: add mungu (darwin)

--- hosts/darwin/aarch64-darwin/mungu/default.nix
+++ hosts/darwin/aarch64-darwin/mungu/default.nix
@@ -6,11 +6,70 @@ in
 {
 
   imports = [
-#TODO: home.nix
+    ../home.nix
   ];
 
-  modules = with config.modules;{
+  modules = with config.modules; {
+
+    darwin = {
+      system.enable = true;
+      #TODO: homebrew.enable = false;
+
+    };
+
+    desktop = {
+      #TODO yabai.enable = false;
+
+
+      media = {
+        mpv.enable = true;
+        graphics = {
+          enable = true;
+          tools.enable = true;
+          photoshop.enable = true;
+          illustrator.enable = true;
+          threed.enable = true;
+        };
+      };
+
+      term  = {
+        default = "kitty";
+        kitty.enable = true;
+      };
+    };
+
+    dev = {
+      enable = true;
+      python.enable = true;
+    };
+
+    editors = {
+      emacs.enable = true;
+      emacs.useForEmail = false;
+      emacs.tlux.enable = true;
+      vim.enable = true;
+      default = editors.emacs.defaultEditor;
+    };
+
+    shell = {
+      direnv.enable = true;
+      git.enable = true;
+      gnupg = {
+        enable = true;
+        useTomb = false;
+        secretKeysPath = "secret_ghelew_net";
+      };
+      tmux.enable = true;
+      zsh.enable = true;
+    };
+
+    theme.active = "nord";
 
   };
+
+  ## SSH config
+  home.file.".ssh/config" = {source = "${configDir}/ssh/config";};
+  #gnupg
+  home.configFile."gnupg/gpg.conf" = {source = "${configDir}/gnupg/gpg.conf";};
 
 }
