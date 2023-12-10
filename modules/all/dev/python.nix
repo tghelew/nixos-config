68913b3934e6e59a97a0b706af452f9f0b36faf1
Thierry Ghélew <tlux@ghelew.net>
2023-12-10 15:18:03 +0100

    config: adpated python module

--- modules/all/dev/python.nix
+++ modules/all/dev/python.nix
@@ -11,6 +11,7 @@ with lib;
 with lib.my;
 let devCfg = config.modules.dev;
     cfg = devCfg.python;
+    configDir = config.nixos-config.configDir;
 in {
   options.modules.dev.python = {
     enable = mkBoolOpt false;
@@ -18,33 +19,46 @@ in {
 
   config = mkMerge [
     (mkIf cfg.enable {
-      user.packages = with pkgs; [
-        python311
-        python311Packages.pip
-        python311Packages.ipython
-        python311Packages.black
-        python311Packages.setuptools
-        python311Packages.pylint
-        poetry
+      user.packages = [
+        (pkgs.python3.withPackages (ps: with ps;
+          [
+            pip
+            ipython
+            black
+            setuptools
+            python-lsp-server
+          ] ++ python-lsp-server.optional-dependencies.all))
       ];
 
       environment.shellAliases = {
         py     = "python";
-        py2    = "python2";
         py3    = "python3";
-        po     = "poetry";
         ipy    = "ipython --no-banner";
         ipylab = "ipython --pylab=qt5 --no-banner";
       };
 
-      env.IPYTHONDIR      = "$XDG_CONFIG_HOME/ipython";
-      env.PIP_CONFIG_FILE = "$XDG_CONFIG_HOME/pip/pip.conf";
-      env.PIP_LOG_FILE    = "$XDG_DATA_HOME/pip/log";
-      env.PYLINTHOME      = "$XDG_DATA_HOME/pylint";
-      env.PYLINTRC        = "$XDG_CONFIG_HOME/pylint/pylintrc";
-      env.PYTHONSTARTUP   = "$XDG_CONFIG_HOME/python/pythonrc";
-      env.PYTHON_EGG_CACHE = "$XDG_CACHE_HOME/python-eggs";
-      env.JUPYTER_CONFIG_DIR = "$XDG_CONFIG_HOME/jupyter";
-    })
+      env.IPYTHONDIR          = "$XDG_CONFIG_HOME/ipython";
+      env.PIP_CONFIG_FILE     = "$XDG_CONFIG_HOME/pip/pip.conf";
+      env.PIP_LOG_FILE        = "$XDG_DATA_HOME/pip/log";
+      env.PYTHONSTARTUP       = "$XDG_CONFIG_HOME/python/pythonrc.py";
+      env.PYTHON_HISTORY_FILE = "$XDG_CACHE_HOME/pythonrc-history";
+      env.PYTHON_EGG_CACHE    = "$XDG_CACHE_HOME/python-eggs";
+      env.JUPYTER_CONFIG_DIR  = "$XDG_CONFIG_HOME/jupyter";
+
+      home.configFile = {
+        pycodestyle.text =
+          ''
+           [pycodestyle]
+            count = False
+            max-line-length = 100
+            ignore = F841
+          '';
+
+        "python/pytonrc.py".source =  "${configDir}/code/python/pythonrc.py";
+
+      };
+    }
+
+    )
  ];
 }
