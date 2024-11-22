69616cada38c42e9dd8e155cb6c1f3a628dc6357
Thierry Ghélew <tlux@ghelew.net>
2024-11-22 11:44:34 +0100

    add yubikey service

---
+++ modules/linux/services/yubikey.nix
@@ -0,0 +1,33 @@
+
+{ options, config, lib, pkgs,... }:
+
+with lib;
+with lib.my;
+let
+  cfg = config.modules.services.yubikey;
+  configDir = config.nixos-config.configDir;
+in {
+  options.modules.services.yubi = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    services.udev.packages = [ pkgs.yubikey-personalization ];
+    programs.gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+    security.pam.yubico = {
+      enable = true;
+      debug = true;
+      mode = "challenge+response";
+    };
+
+  environment.systemPackages = with pkgs; [
+    pam_u2f
+    yubikey-personalisation
+  ];
+
+
+  };
+}
